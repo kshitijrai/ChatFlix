748b1a410118a4a64634d43d90a044210f7a1bbe
kshitijrai <40623816+kshitijrai@users.noreply.github.com>
2021-02-04 01:39:07 +0530

    SignIn Screen Updated

--- lib/helperfunctions/slidermodels.dart
+++ lib/helperfunctions/slidermodels.dart
@@ -1,18 +1,11 @@
-import 'package:flutter/material.dart';
-
 class SliderModel {
   String imageAssetPath;
-  String title;
   String desc;
 
-  SliderModel({this.imageAssetPath, this.title, this.desc});
+  SliderModel({this.imageAssetPath, this.desc});
 
   void setImageAssetPath(String getImageAssetPath) {
     imageAssetPath = getImageAssetPath;
-  }
-
-  void setTitle(String getTitle) {
-    title = getTitle;
   }
 
   void setDesc(String getDesc) {
@@ -21,10 +14,6 @@ class SliderModel {
 
   String getImageAssetPath() {
     return imageAssetPath;
-  }
-
-  String getTitle() {
-    return title;
   }
 
   String getDesc() {
@@ -38,27 +27,23 @@ List<SliderModel> getSlides() {
 
   //1
   sliderModel.setDesc(
-      "Discover Restaurants offering the best fast food food near you on Foodwa");
-  sliderModel.setTitle("Search");
-  sliderModel.setImageAssetPath("assets/illustration.png");
+      "Flutter is Google’s UI toolkit for building beautiful, natively compiled applications for mobile, web, and desktop from a single codebase.");
+  sliderModel.setImageAssetPath("assets/flutter.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
 
   //2
   sliderModel.setDesc(
-      "Our veggie plan is filled with delicious seasonal vegetables, whole grains, beautiful cheeses and vegetarian proteins");
-  sliderModel.setTitle("Order");
-  sliderModel.setImageAssetPath("assets/illustration2.png");
+      "Uses Firebase Services like Authentication for user signin via Google SignIn, Cloud Firestore -  flexible, scalable database for mobile, web, and server development from Firebase and Google Cloud.");
+  sliderModel.setImageAssetPath("assets/firebase.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
 
   //3
-  sliderModel.setDesc(
-      "Food delivery or pickup from local restaurants, Explore restaurants that deliver near you.");
-  sliderModel.setTitle("Eat");
-  sliderModel.setImageAssetPath("assets/illustration3.png");
+  sliderModel.setDesc("Search user via username, Send user message real time!");
+  sliderModel.setImageAssetPath("assets/cloud-sync.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp/helperfunctions/slidermodels.dart';
 import 'package:chatapp/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,29 +8,151 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  List<SliderModel> mySLides = new List<SliderModel>();
+  int slideIndex = 0;
+  PageController controller;
+
+  Widget _buildPageIndicator(bool isCurrentPage) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: isCurrentPage ? 10.0 : 6.0,
+      width: isCurrentPage ? 10.0 : 6.0,
+      decoration: BoxDecoration(
+        color: isCurrentPage ? Colors.grey : Colors.grey[300],
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    mySLides = getSlides();
+    controller = new PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ChatFlix"),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            AuthMethods().signInWithGoogle(context);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(24),
-              color: Color(0xffDB4437),
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Text(
-              "Sign In with Google",
-              style: TextStyle(fontSize: 16, color: Colors.white),
-            ),
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [const Color(0xff3C8CE7), const Color(0xff00EAFF)])),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          height: MediaQuery.of(context).size.height - 100,
+          child: PageView(
+            controller: controller,
+            onPageChanged: (index) {
+              setState(() {
+                slideIndex = index;
+              });
+            },
+            children: <Widget>[
+              SlideTile(
+                imagePath: mySLides[0].getImageAssetPath(),
+                desc: mySLides[0].getDesc(),
+              ),
+              SlideTile(
+                imagePath: mySLides[1].getImageAssetPath(),
+                desc: mySLides[1].getDesc(),
+              ),
+              SlideTile(
+                imagePath: mySLides[2].getImageAssetPath(),
+                desc: mySLides[2].getDesc(),
+              )
+            ],
           ),
         ),
+        bottomSheet: slideIndex != 2
+            ? Container(
+                margin: EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        controller.animateToPage(2,
+                            duration: Duration(milliseconds: 400),
+                            curve: Curves.linear);
+                      },
+                      splashColor: Colors.blue[50],
+                      child: Text(
+                        "SKIP",
+                        style: TextStyle(
+                            color: Color(0xFF0074E4),
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          for (int i = 0; i < 3; i++)
+                            i == slideIndex
+                                ? _buildPageIndicator(true)
+                                : _buildPageIndicator(false),
+                        ],
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        controller.animateToPage(slideIndex + 1,
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.linear);
+                      },
+                      splashColor: Colors.blue[50],
+                      child: Text(
+                        "NEXT",
+                        style: TextStyle(
+                            color: Color(0xFF0074E4),
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : InkWell(
+                onTap: () {
+                  AuthMethods().signInWithGoogle(context);
+                },
+                child: Container(
+                  height: 60,
+                  color: Color(0xffDB4437),
+                  alignment: Alignment.center,
+                  child: Text(
+                    "SIGN IN WITH GOOGLE",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class SlideTile extends StatelessWidget {
+  String imagePath, desc;
+
+  SlideTile({this.imagePath, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(imagePath),
+          SizedBox(
+            height: 40,
+          ),
+          Text(desc,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14))
+        ],
       ),
     );
   }
